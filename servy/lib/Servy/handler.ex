f814a089c82fa8384a9a38d9f11b8b2af2b1817b
Daron Spence <4062087+Daronspence@users.noreply.github.com>
2021-09-30 20:33:36 -0500

    stuff

--- servy/lib/Servy/handler.ex
+++ servy/lib/Servy/handler.ex
@@ -1,6 +1,10 @@
 defmodule Servy.Handler do
   @moduledoc "Handles HTTP requests."
-  @pagespath Path.expand("../../pages", __DIR__)
+  alias Servy.Conv
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Servy.Parser
+  @pages_path Path.expand("../../pages", __DIR__)
+
   @doc "Transforsm the request into a response."
   def handle(request) do
     request
@@ -12,52 +16,31 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def track(%{status: 404, path: path} = conv) do
-    IO.puts("Warning, #{path} is on the loose!\n")
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{conv | path: "/wildthings"}
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def log(conv), do: IO.inspect(conv)
-
-  def parse(request) do
-    [method, path, _version] =
-      request
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
-
-    %{method: method, path: path, resp_body: "", status: nil}
-  end
-
-  def route(%{method: "GET", path: "/wildthings"} = conv) do
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
     %{conv | resp_body: "Bears, Lions, Tigers", status: 200}
   end
 
-  def route(%{method: "GET", path: "/bears"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
     %{conv | resp_body: "Teddy, Smoky, Paddington", status: 200}
   end
 
-  def route(%{method: "GET", path: "/bears/" <> _id} = conv) do
+  def route(%Conv{method: "GET", path: "/bears/" <> _id} = conv) do
     %{conv | resp_body: "Paddington", status: 200}
   end
 
-  def route(%{method: "GET", path: "/about"} = conv) do
-    @pagespath
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    %{conv | status: 201, resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}!"}
+  end
+
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
+    @pages_path
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
   end
 
   def route(%{method: _method, path: path} = conv) do
-    %{conv | resp_body: "No #{path} here!", status: 404}
+    %Conv{conv | resp_body: "No #{path} here!", status: 404}
   end
 
   def handle_file({:ok, content}, conv), do: %{conv | status: 200, resp_body: content}
@@ -67,25 +50,14 @@ defmodule Servy.Handler do
   def handle_file({:error, reason}, conv),
     do: %{conv | status: 404, resp_body: "File error: #{reason}"}
 
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
-  end
-
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }[code]
   end
 end
 
@@ -179,6 +151,23 @@ Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+IO.puts("-------------------------")
+
+request = """
+POST /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 21
+
+name=Baloo&type=Brown
 """
 
 response = Servy.Handler.handle(request)
