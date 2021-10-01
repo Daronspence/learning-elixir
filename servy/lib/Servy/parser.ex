f0a483d9a9c090a5c34285d7b990a397679e3140
Daron Spence <4062087+Daronspence@users.noreply.github.com>
2021-09-30 21:18:51 -0500

    Remove code from course (maybe iex will be faster?)

--- servy/lib/Servy/parser.ex
+++ servy/lib/Servy/parser.ex
@@ -1,26 +1,39 @@
 defmodule Servy.Parser do
-
   alias Servy.Conv
 
   def parse(request) do
     [top, params_string] = String.split(request, "\n\n")
 
-    [reaquest_line | _header_lines] = String.split(top, "\n")
+    [reaquest_line | header_lines] = String.split(top, "\n")
 
     [method, path, _version] = String.split(reaquest_line, " ")
 
-    params = parse_params(params_string)
+    headers = parse_headers(header_lines, %{})
+
+    params = parse_params(headers["Content-Type"], params_string)
 
     %Conv{
       method: method,
       path: path,
       params: params,
+      headers: headers,
     }
   end
 
-  defp parse_params(params) do
+  defp parse_params("application/x-www-form-urlencoded", params) do
     params
-    |> String.trim
-    |> URI.decode_query
+    |> String.trim()
+    |> URI.decode_query()
   end
+
+  defp parse_params(_, _), do: %{}
+
+  defp parse_headers([head | tail], %{} = headers) do
+    [key, value] = String.split(head, ": ")
+    headers = Map.put(headers, key, value)
+    parse_headers(tail, headers)
+  end
+
+  defp parse_headers([], %{} = headers), do: headers
+
 end
